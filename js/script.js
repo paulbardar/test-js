2676652da092ecc202357b8d238328e65c7c0487
Pavel <testemail@gmail.com>
2023-03-15 18:09:31 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+;(function(){
+"use strict";
+
+
+// const person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     age: 25,
+//     isMaried: false,
+//     id     :  5566
+// };
+
+// console.log(person.firstName);
+// console.log(person["firstName"]);
+
+ // 9 Lesson 
+    // console.log("arr" + " - object111");
+    // console.log(4 + +"5");
+
+    let incr = 10,
+        decr = 10;
+    
+
+    // ++incr;
+    // --decr;
+
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log(2 + 2 * 2 === 8);
+
+    const isChecked = true,
+          isClose = false;
+    
+    console.log(isChecked || isClose);
+
+
+})();
